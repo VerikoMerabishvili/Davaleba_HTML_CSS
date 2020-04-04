c85fa0edb4b8d5456aa4d7a2d33c1d109e71efb5
Your Name <merabishvili.veriko@gmail.com>
2020-04-04 18:17:38 +0400

    first upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+
+</head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<style>
+
+
+</style>
+
+<body>
+
+
+
+
+    <nav class="menu">
+        <ul class="menu-list"  id="menu1">
+            <div  class="item" id="blog">blog template </div>
+            <li class="item">about</li>
+            <li class="item"> cooperation</li>
+            <li class="item"> contact</li>
+
+                 </ul>
+
+                <ul id="menu2"class="menu-list" >
+            <li class="item">nature</li>
+            <li class="item">peope</li>
+            <li class="item">trips</li>
+            <li class="item">animals</li>
+            <li class="item">fashion</li>
+            <li class="item">tech</li>
+            <li class="item">...</li>
+
+
+        </ul>
+    </nav>
+
+
+
+
+</body>
+
+</html>
