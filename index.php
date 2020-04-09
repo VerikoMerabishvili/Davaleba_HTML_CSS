6c60e52f7165ca828f8a96cf17b8d400161a414f
Your Name <merabishvili.veriko@gmail.com>
2020-04-09 16:21:36 +0400

    saboloo versia

--- index.php
+++ index.php
@@ -3,8 +3,351 @@
 <head>
 
 </head>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+
 <style>
+    #s1 {
+        width: 390px;
+        height: 200;
+        position: absolute;
+        top: 600px;
+        left: 200px;
+    }
+
+    #s2 {
+        width: 390px;
+        height: 200;
+        position: absolute;
+        top: 600px;
+        right: 200px;
+    }
+
+    #s3 {
+
+
+        position: absolute;
+        right: 200px;
+        top: 950px;
+        width: 950px
+    }
+
+    #s4 {
+        position: absolute;
+        top: 1300px;
+        left: 200px;
+        width: 250px
+    }
+
+
+    #s5 {
+        position: absolute;
+        top: 1300px;
+        left: 555px;
+        width: 250px
+    }
+
+    #s6 {
+        position: absolute;
+        top: 1300px;
+        right: 200px;
+        width: 250px
+    }
+
+    #s7 {
+        position: absolute;
+        top: 1600px;
+        left: 200px;
+        width: 250px
+    }
+
+
+    #s8 {
+        position: absolute;
+        top: 1600px;
+        left: 555px;
+        width: 250px
+    }
+
+
+
+    #s9 {
+        position: absolute;
+        top: 1600px;
+        right: 200px;
+        width: 250px
+    }
+
+    #divv1 {
+        display: block;
+        position: absolute;
+        font-size: 20px;
+        top: 800px;
+        left: 200px;
+    }
+
+    #divv2 {
+        display: block;
+        position: absolute;
+        font-size: 20px;
+        top: 800px;
+        left: 760px
+    }
+
+    #divv3 {
+
+        position: absolute;
+        top: 1480px;
+        left: 200px;
+        width: 250px
+    }
+
+    #divv4 {
+        position: absolute;
+        top: 1480px;
+        left: 555px;
+        width: 250px
+    }
+
+    #divv5 {
+
+        position: absolute;
+        top: 1480px;
+        left: 900px;
+        width: 250px
+    }
+
+
+
+    #divv6 {
+
+        position: absolute;
+        top: 1800px;
+        left: 900px;
+        width: 250px
+    }
+
+
+    #divv7 {
+
+        position: absolute;
+        top: 1800px;
+        left: 550px;
+        width: 250px
+    }
+
+
+    #divv8 {
+
+        position: absolute;
+        top: 1800px;
+        left: 200px;
+        width: 250px
+    }
+
+
+    #ss1 {
+        width: 390px;
+        height: 200;
+        position: absolute;
+        top: 2000px;
+        left: 200px;
+    }
+
+    #ss2 {
+        width: 390px;
+        height: 200;
+        position: absolute;
+        top: 2000px;
+        right: 200px;
+    }
+
+    #div1 {
+        display: block;
+        position: absolute;
+        font-size: 20px;
+        top: 2210px;
+        left: 200px
+    }
+
+    #div2 {
+        display: block;
+        position: absolute;
+        font-size: 20px;
+        top: 2210px;
+        left: 760px
+    }
+
+    #bluelast{
+        background-color: #1353E8 ; 
+        width: 100%;
+        height: 200px;
+        position: absolute;
+        top: 2400;
+    }
+
+    .new{
+        margin-top: 70px;
+        color: white;
+        text-align: center
+    }
+
+    .new2{
+color: white;
+text-align: center;
+opacity: 0.2;
+    }
+
+
+.last1{
+position: absolute;
+top: 2700;
+right:300;
+  height: 26.5px;
+  font-family: ProximaNova;
+  font-size: 20px;
+  font-weight: 300;
+
+  text-align: center;
+  color: #a7acbc;
+}
+
+.last2{
+    position: absolute;
+top: 2750;
+right:500;
+  height: 26.5px;
+  font-family: ProximaNova;
+  font-size: 20px;
+  font-weight: 300;
+
+  text-align: center;
+  color: #a7acbc;
+
+}
+
+.trips{
+    background-color:#DA0043;
+    position: absolute;
+    top: 750;
+    left: 200;
+    width: 50;
+    height: 30;
+    color: white;
+}
+.trips2{
+    background-color:#DA0043;
+    position: absolute;
+    top: 1200;
+    left: 200;
+    width: 50;
+    height: 30;
+    color: white;
+
+
+
+}
+.tech{
+    background-color:black;
+    position: absolute;
+    top: 750;
+    left: 760;
+    width: 50;
+    height: 30;
+    color: white;
+
+}
+
+
+.nature{
+    background-color:#57B700;
+    position: absolute;
+    top: 210;
+    left:0;
+    width: 50;
+    height: 30;
+    color: white;
+}
+
+
+.people3{
+    background-color:#1353E8;
+    position: absolute;
+    top: 210;
+    left: 699;
+    width: 50;
+    height: 30;
+    color: white;
+}
+
+
+.tech3{
+    background-color:black;
+    position: absolute;
+    top: 210;
+    left: 355;
+    width: 50;
+    height: 30;
+    color: white;
+}
+
+.trips3{
+    background-color:#DA0043;
+    position: absolute;
+    top: 520;
+    left: 355;
+    width: 50;
+    height: 30;
+    color: white;
+}
+
+.trips4{
+    background-color:#DA0043;
+    position: absolute;
+    top: 520;
+    left: 0;
+    width: 50;
+    height: 30;
+    color: white;
+}
+
+
+.fashion{
+    background-color:#8300B3;
+    position: absolute;
+    top: 520;
+    left: 699;
+    width: 60;
+    height: 30;
+    color: white;
+
+
+}
+
+.tripss{
+    background-color:#DA0043;
+    position: absolute;
+    top: 950;
+    left: 0;
+    width: 50;
+    height: 30;
+    color: white;
+
+}
+
+.techh{
+    background-color:black;
+    position: absolute;
+    top: 950;
+    left: 560;
+    width: 50;
+    height: 30;
+    color: white;
+
+
+}
+
+
+
 
 
 </style>
@@ -15,26 +358,230 @@
 
 
     <nav class="menu">
-        <ul class="menu-list"  id="menu1">
-            <div  class="item" id="blog">blog template </div>
-            <li class="item">about</li>
-            <li class="item"> cooperation</li>
-            <li class="item"> contact</li>
+        <ul class="menu-list" id="menu1">
 
-                 </ul>
-
-                <ul id="menu2"class="menu-list" >
-            <li class="item">nature</li>
-            <li class="item">peope</li>
-            <li class="item">trips</li>
-            <li class="item">animals</li>
-            <li class="item">fashion</li>
-            <li class="item">tech</li>
-            <li class="item">...</li>
-
+            <li class="item gray">About</li>
+            <li class="item gray"> Cooperation</li>
+            <li class="item gray"> Contact</li>
 
         </ul>
+
+        <ul id="menu2" class="menu-list">
+            <li class="item blu">Nature</li>
+            <li class="item blu">Peope</li>
+            <li class="item blu">Trips</li>
+            <li class="item blu">Animals</li>
+            <li class="item blu">Fashion</li>
+            <li class="item blu">Tech</li>
+            <li class="item blu">...</li>
+
+        </ul>
+        <div class="collor">
+            <h4 class="bgt">Blog Template</h4>
+        </div>
     </nav>
+
+    <div>
+        <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/937B4627-FF01-4674-BE07-95E911D87627.png" alt="" style="width: 100%">
+    </div>
+
+
+
+    <h1 id="text1">Maecenas quis lobortis nunc. Nullam sit<br>
+        vel odio congue odio congue vulputate a ut nisi</h1>
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/D793EEDE-6FA2-4895-B975-83068496D7ED.png" alt="" id="pic1">
+
+    <div class="people">
+        <h5 class="people2">people</h2>
+    </div><br>
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/E0BA6155-4FF2-4E82-88E3-97163B3A9CE4.png" id="s1">
+
+
+
+
+    <div id="divv1">
+        <span class="span1">Donec elementum dui semper, pretium dui quis,<br> pretium nisl.</span><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/E0BA6155-4FF2-4E82-88E3-97163B3A9CE4.png" id="s2">
+
+
+    <div id="divv2">
+        <span class="span1">Donec elementum dui semper, pretium dui quis,<br> pretium nisl.</span><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/EB584EA9-4B30-4B02-BBA5-E55134D6C6FD.png" id="s3">
+
+
+
+
+
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/D66D8779-E3BC-4BE4-9E53-974E49E1CD4F.png" alt="" id="s4">
+    <div id="divv3">
+        <span class="span1">Donec-elementum-dui-semper-pretium-dui-quis-pretium-nisl</span><br><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+
+
+
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/1D720150-8CA8-44FA-958C-A1B978E8950C.png" alt="" id="s5">
+    <div id="divv4">
+        <span class="span1">Donec-elementum-dui-semper-pretium-dui-quis-pretium-nisl</span><br><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/D089C4AF-4201-4875-B511-358999AD6ACD.png" alt="" id="s6">
+    <div id="divv5">
+        <span class="span1">Donec-elementum-dui-semper-pretium-dui-quis-pretium-nisl</span><br><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+
+
+
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/E0C0ECAA-2546-44E8-846A-677B09D5944F.png" id="s7">
+    <div id="divv6">
+        <span class="span1">Donec-elementum-dui-semper-pretium-dui-quis-pretium-nisl</span><br><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/8AE69EEC-D62B-4163-93EA-C8A783EEE011.png" id="s8">
+    <div id="divv7">
+        <span class="span1">Donec-elementum-dui-semper-pretium-dui-quis-pretium-nisl</span><br><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+
+    
+    <img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/EE35EB45-2646-46E9-A6C7-4636100D0605.png" id="s9">
+    <div id="divv8">
+        <span class="span1">Donec-elementum-dui-semper-pretium-dui-quis-pretium-nisl</span><br><br>
+        <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+        <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+    </div>
+
+
+
+
+
+
+
+<img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/E0BA6155-4FF2-4E82-88E3-97163B3A9CE4.png" id="ss1">
+
+<div id="div1">
+    <span class="span1">Donec elementum dui semper, pretium dui quis,<br> pretium nisl.</span><br>
+    <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+    <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+</div>
+
+
+
+<img src="https://cdn.zeplin.io/5e885c7105ca2e21c637e6fb/assets/E0BA6155-4FF2-4E82-88E3-97163B3A9CE4.png" id="ss2">
+
+
+<div id="divv2">
+    <span class="span1">Donec elementum dui semper, pretium dui quis,<br> pretium nisl.</span><br>
+    <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+    <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+</div>
+
+<div id="div2">
+    <span class="span1">Donec elementum dui semper, pretium dui quis,<br> pretium nisl.</span><br>
+    <span class="span2" style="color: gray; font-size:15px">11 september 2016 |</span>
+    <a class="span2" href="#JohnDoe" style="font-size:15px">John Doe</a><br>
+</div>
+
+
+
+
+<div id="bluelast">
+
+<h1 class="new">NewsLetter</h1><br>
+<h3 class="new2">Donec elementum dui semper, pretium dui quis,pretium nisl.</h3>
+
+</div>
+
+
+<div>
+<h2 class="last1">Cum sociis natoque penabitus et magnis dis parturient montes, nascetur ridiculus mus.</h2>
+<h3 class="last2">Try awesome tool for dedsgners.   symu.co</h3><br><br><br>
+</div>
+
+
+
+
+
+
+<div class="trips">
+<h4 style="text-align:center">Trips</h2>
+
+
+</div>
+<div class="tech">
+<h4 style="text-align:center">Tech</h2>
+</div>
+
+<div class="trips2">
+<h4 style="text-align:center">Trips</h2>
+
+
+
+<div class="nature">
+    <h4>nature</h4>
+</div>
+
+<div class="people3">
+    <h4>people</h4>
+</div>
+    
+<div class="tech3">
+    <h4>tech</h4>
+</div>
+
+
+
+
+
+<div class="trips3">
+<h4 style="text-align:center">Trips</h2>
+</div>
+
+<div class="trips4">
+<h4 style="text-align:center">Trips</h2>
+</div>
+
+
+<div class="fashion">
+<h4 style="text-align:center">Fashion</h2>
+</div>
+
+
+
+<div class="tripss">
+<h4 style="text-align:center">Trips</h2>
+
+
+</div>
+<div class="techh">
+<h4 style="text-align:center">Tech</h2>
+</div>
+
+
+
+
+
 
 
 
